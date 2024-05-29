07fc6b12ef26c52954a00d5064661834ef238a6b
Максим <sterh84@gmail.com>
2024-05-29 09:56:56 +0300

    поправил offer-mock

--- src/mock/offer-mock.js
+++ src/mock/offer-mock.js
@@ -4,14 +4,14 @@ import { EVENT_TYPES } from './variablies.js';
 const OFFER_TITLES = ['Order meal', 'Infotainment system', 'Choose seats', 'Book a taxi at the arrival point', 'Wake up at a certain time'];
 
 const createOffer = () => ({
-	id: crypto.randomUUID(),
-	title: getRandomArrayElement(OFFER_TITLES),
-	price: getRandomInt(500),
+  id: crypto.randomUUID(),
+  title: getRandomArrayElement(OFFER_TITLES),
+  price: getRandomInt(500),
 });
 
 const offers = EVENT_TYPES.map((item) => ({
-	type: item,
-	offer: Array.from({ length: getRandomInt(6) }, createOffer),
+  type: item,
+  offer: Array.from({ length: getRandomInt(6) }, createOffer),
 }));
 
 export const createRandomOffers = () => offers;
