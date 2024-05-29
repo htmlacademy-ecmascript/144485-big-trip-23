97c0158bb74baf4a9b9ff257f7cdfe265b503cec
Arthur Baileys Li <arthurlitovko@gmail.com>
2024-05-29 20:59:06 +0300

    Fix eslint errors

--- src/utils.js/day.js
+++ src/utils.js/day.js
@@ -1,7 +1,7 @@
-import duration from 'dayjs/plugin/duration';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import localizedFormat from 'dayjs/plugin/localizedFormat';
 import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration.js';
+import localizedFormat from 'dayjs/plugin/localizedFormat.js';
+import relativeTime from 'dayjs/plugin/relativeTime.js';
 
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
--- src/view/waypoint-edit.js
+++ src/view/waypoint-edit.js
@@ -97,30 +97,30 @@ const createWaypointForm = (waypoint, destinationAll, offersModelAll) => {
   <section class="event__details">
     <section class="event__section  event__section--offers">
     ${
-      offerList.length > 0
-        ? `
+  offerList.length > 0
+    ? `
       <h3 class="event__section-title  event__section-title--offers" > Offers</h3 >
         <div class="event__available-offers">
           ${offerList}
         </div>
         `
-        : ''
-    }
+    : ''
+}
     </section >
 
   <section class="event__section  event__section--destination">
     <h3 class="event__section-title  event__section-title--destination">${destinationName}</h3>
     <p class="event__destination-description">${destinationDescription}</p>
     ${
-      photoItems.length > 0
-        ? `
+  photoItems.length > 0
+    ? `
       <div class="event__photos-container">
         <div class="event__photos-tape">
           ${photoItems}
         </div>
       </div> `
-        : ''
-    }
+    : ''
+}
 
 
   </section >
